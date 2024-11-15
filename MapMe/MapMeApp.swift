87722d202112751201347766c44df11472f61b9a
Fr3shTricks <fr3shtricks@gmail.com>
2024-11-14 21:01:44 -0800

    Initial Commit

---
+++ MapMe/MapMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapMeApp.swift
+//  MapMe
+//
+//  Created by Crypto on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MapMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
